86c1441da3ee66aeaaa4638befb7b3ca1e8d5f1d
yashi8484 <abok@yashi84.com>
2023-09-03 18:49:33 +0900

    Section 5.5.2

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.get("/", (req, res, next) => {
 });
 app.get("/csp", (req, res) => {
   const nonceValue = crypto.randomBytes(16).toString("base64");
-  res.header("Content-Security-Policy", `script-src 'nonce-${nonceValue}'`);
+  res.header(
+    "Content-Security-Policy",
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none';"
+  );
   res.render("csp", { nonce: nonceValue });
 });
 
