f4720f994aaafc05d95edfd13b2aee07e2dd729b
yashi8484 <abok@yashi84.com>
2023-09-03 18:41:57 +0900

    Section 5.5.1

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+const crypto = require("crypto");
 const express = require("express");
 const api = require("./routes/api");
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use("/api", api);
 
 app.get("/", (req, res, next) => {
   res.end("Top Page");
+});
+app.get("/csp", (req, res) => {
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+  res.header("Content-Security-Policy", `script-src 'nonce-${nonceValue}'`);
+  res.render("csp", { nonce: nonceValue });
 });
 
 app.listen(port, () => {
