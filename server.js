f63644e158dd66aba9e680ecea26735eb1728c7b
yashi8484 <abok@yashi84.com>
2023-09-09 16:51:12 +0900

    Section 6.4

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      res.header("X-Frame-Options", "SAMEORIGIN");
+    },
+  })
+);
 app.use("/api", api);
 app.use("/csrf", csrf);
 
